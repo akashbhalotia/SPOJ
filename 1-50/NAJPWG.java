01454de7ac72071d70cb5bce0b5f1a89e7b64b1f
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-07-05 00:32:42 +0530

    Create NAJPWG.java

---
+++ 1-50/NAJPWG.java
@@ -0,0 +1,54 @@
+// Created by akashbhalotia
+import java.io.*;
+import java.util.*;
+class A
+{
+    private static int solve(int N)
+    {
+        int res=N,i;
+
+        for(i=2;i*i<=N;i++)
+        {
+            if(N%i==0)
+            {
+                while(N%i==0)
+                    N/=i;
+                res-=res/i;
+            }
+        }
+        if(N>1)
+            res-=res/N;
+        return res;
+    }
+    public static void main(String[] args) throws IOException
+    {
+        Reader r=new Reader();
+
+        int i,N;
+        int T=r.nextInt();
+        StringBuilder sb=new StringBuilder();
+        long sum[]=new long[100005];
+
+        for(i=2;i<=100000;i++)
+            sum[i]=sum[i-1]+i-solve(i);
+
+        for(i=1;i<=T;i++)
+        {
+            N=r.nextInt();
+            sb.append("Case ").append(i).append(':').append(" ").append(sum[N]).append("\n");
+        }
+        System.out.println(sb);
+    }
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;private DataInputStream din;private byte[] buffer;private int bufferPointer, bytesRead;
+        public Reader(){din=new DataInputStream(System.in);buffer=new byte[BUFFER_SIZE];bufferPointer=bytesRead=0;
+        }public Reader(String file_name) throws IOException{din=new DataInputStream(new FileInputStream(file_name));buffer=new byte[BUFFER_SIZE];bufferPointer=bytesRead=0;
+        }public String readLine() throws IOException{byte[] buf=new byte[64];int cnt=0,c;while((c=read())!=-1){if(c=='\n')break;buf[cnt++]=(byte)c;}return new String(buf,0,cnt);
+        }public int nextInt() throws IOException{int ret=0;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');if(neg)c=read();do{ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(neg)return -ret;return ret;
+        }public long nextLong() throws IOException{long ret=0;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');if(neg)c=read();do{ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(neg)return -ret;return ret;
+        }public double nextDouble() throws IOException{double ret=0,div=1;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');if(neg)c = read();do {ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(c=='.')while((c=read())>='0'&&c<='9')ret+=(c-'0')/(div*=10);if(neg)return -ret;return ret;
+        }private void fillBuffer() throws IOException{bytesRead=din.read(buffer,bufferPointer=0,BUFFER_SIZE);if(bytesRead==-1)buffer[0]=-1;
+        }private byte read() throws IOException{if(bufferPointer==bytesRead)fillBuffer();return buffer[bufferPointer++];
+        }public void close() throws IOException{if(din==null) return;din.close();}
+    }
+}
